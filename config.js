1654a35db11e63e6d8128cfce85f55c6d64084bd
57 <mrrfvg@gmail.com>
2018-11-05 17:39:49 +0800

    init blog

---
+++ config.js
@@ -0,0 +1,10 @@
+config = {
+    blog_name: "不愉快✱🔨",
+    github_username: "micky619",       // github 账号名
+    github_repo: "micky619.github.io", // github 建立的 repo 名
+    per_page: 10,                      // 首页每次载入文章列表数量
+
+    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
+    // 将配置好的 access token 拆分成两个字符串相加
+    access_token: "25a9fef2bf44253a6a97852979889a756c9c49d6"
+};
