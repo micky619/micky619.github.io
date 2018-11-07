119406bf71e1a2d5e0db038f95022c96f1bcbaff
57 <mrrfvg@gmail.com>
2018-11-07 11:13:46 +0800

    init blog v2

--- config.js
+++ config.js
@@ -1,10 +1,20 @@
-config = {
-    blog_name: "不愉快✱🔨",
-    github_username: "micky619",       // github 账号名
-    github_repo: "micky619.github.io", // github 建立的 repo 名
-    per_page: 10,                      // 首页每次载入文章列表数量
-
-    // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
-    // 将配置好的 access token 拆分成两个字符串相加
-    access_token: "25a9fef2bf44253a6a97" + "852979889a756c9c49d6"
-};
+window.config = {
+  server: 'https://api.github.com',
+  siteName: '不愉快✱',
+  navbar: [
+    {
+      label: '首页',
+      link: '/'
+    }, {
+      label: '博客',
+      link: '/fuyukai_issue_blog'
+    }, {
+      label: 'react',
+      link: '/fuyukai_issue_blog/tag/enhancement'
+    }
+  ],
+  github: {
+    user: 'micky619',
+    repo: 'fuyukai_issue_blog'
+  }
+}
