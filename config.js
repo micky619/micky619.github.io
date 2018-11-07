5a248c2bf314e6cb410379338962d2babfe03b93
57 <mrrfvg@gmail.com>
2018-11-07 10:16:36 +0800

    add access_token

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ config = {
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     // 将配置好的 access token 拆分成两个字符串相加
-    access_token: "25a9fef2bf44253a6a97852979889a756c9c49d6"
+    access_token: "25a9fef2bf44253a6a97" + "852979889a756c9c49d6"
 };
